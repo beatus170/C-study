f66dfb1ff9f270a5d357233ed9e862e3c9ec9825
Beatus170 <beatus170@gmail.com>
2025-12-06 21:46:03 +0900

    Add proift calculator code

---
+++ profit.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+	int sales;
+	int cost;
+	int profit;
+	int a;
+
+	while(1) {
+
+	printf("\nInput Sales : ");
+	scanf("%d", &sales);
+
+	printf("Input Cost : ");
+	scanf("%d", &cost);
+
+	profit = sales - cost;
+
+	printf("Profit is %d Won.\n", profit);
+
+	printf("\nPress 0 to Exit: ");
+	scanf("%d", &a);
+
+	if( a == 0 ) {
+		break;
+	}else continue;
+
+	}	
+
+	return 0;
+
+}
+
+
+
