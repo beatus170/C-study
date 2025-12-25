e70b9abd90e1d622b62805f129c11c79653d5f90
Beatus170 <beatus170@gmail.com>
2025-12-26 08:20:19 +0900

    5. pointer.c update

---
+++ 5. pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>     
+                               
+int main(){
+                                                                     
+  int energe = 100;                                                                                            
+  int *ptr = &energe;    
+                                                                                             
+  printf("수정전값 : %d\n", energe);    
+                                                                      *ptr = 50;  
+                                                                                                                    
+  printf("수정후 값 : %d\n", energe);        
+  printf("수정후 값 : %d\n",* ptr);  
+  printf(" 값 주소 : %p\n",(void*) ptr); 
+       
+        return 0;                                     
+}
