3496c3e0db8991221d94371b06c524b4e2a165ff
Beatus170 <beatus170@gmail.com>
2025-12-26 07:57:20 +0900

    5. array_ptr.c update

---
+++ 5. array_ptr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int values[3] = {100, 200, 300};
+
+        printf("First element (index 0): %d\n", *values);
+
+        printf("Second element (index 1): %d\n", *(values + 1));
+
+              return 0;
+}
