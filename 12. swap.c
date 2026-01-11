ba44d884753a11e11ee63e922c3855bc5fbe3a12
Beatus170 <beatus170@gmail.com>
2026-01-12 07:46:09 +0900

    12. swap.c update

---
+++ 12. swap.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void swap(int *n1, int *n2) {
+
+        int temp;
+
+        temp = *n1;
+        *n1 = *n2;
+        *n2 = temp;
+}
+
+int main() {
+
+        int a = 10;
+        int b = 20;
+
+        printf("Before swap : a = %d, b = %d\n", a, b);
+
+        swap (&a, &b) ;
+
+        printf("After  swap : a = %d, b = %d\n", a, b);
+
+        return 0;
+}
